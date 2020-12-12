7f74fb9878382738a954027102812712d065ab7b
dzmitry nayezzhy <d.nayezzhy@gmail.com>
2020-12-12 13:52:13 +0300

    fix: countries table

--- src/index.js
+++ src/index.js
@@ -79,7 +79,24 @@ async function getData(category, country) {
 function globalChange(summaryData) {
     if (!summaryData) summaryData = JSON.parse(localStorage.getItem(summary));
     let lastUpdate = new Date(summaryData.Date);
-    blockCountriesList.innerText = `${summaryData.Global.TotalConfirmed} ${lastUpdate.toLocaleString()}`;
+    let tr = {};
+    let td = {};
+    summaryData = JSON.parse(localStorage.getItem(summary));
+    let TatalCases = create('div', 'total-cases', `TotalCases: ${summaryData.Global.TotalConfirmed} Last Update: ${lastUpdate.toLocaleString()}`, blockCountriesList);
+    let table = create('table', 'countries-table', null, blockCountriesList);
+    let thCountry = create('td', null, 'Country');
+    let thTotal = create('td', null, 'Total');
+    let tdNew = create('td', null, 'New today');
+    let th = create('th', 'table-header', [thTotal, thCountry], table);
+    summaryData.Countries.forEach((country, i) => {
+        console.log(country.Country, country.NewConfirmed)
+        tr[i] = create('tr', 'country-row', null, table);
+        td = create('td', 'total-confirmed', String(country.TotalConfirmed), tr[i] );
+        td = create('td', 'country', country.Country, tr[i] );
+        tr[i].addEventListener('click', () => {});
+    })
+    thCountry.addEventListener('click', () => {});
+    thTotal.addEventListener('click', () => {});
 }
 
 function countryChange(countryData) {
@@ -89,22 +106,7 @@ function countryChange(countryData) {
 }
 
 function countriesChange(summaryData) {
-    let tr = {};
-    let td = {};
-    summaryData = JSON.parse(localStorage.getItem(summary));
-    let table = create('table', 'countries-table', null, blockTable);
-    let tdCountry = create('td', null, 'Country');
-    let tdTotal = create('td', null, 'Total');
-    let tdNew = create('td', null, 'New today');
-    let th = create('th', 'table-header', [tdCountry, tdNew, tdTotal], table);
-    summaryData.Countries.forEach((country, i) => {
-        console.log(country.Country, country.NewConfirmed)
-        tr[i] = create('tr', 'country-row', null, table);
-        td = create('td', 'country', country.Country, tr[i] );
-        td = create('td', 'total-confirmed', String(country.TotalConfirmed), tr[i] );
-        td = create('td', 'new-confirmed', String(country.NewConfirmed), tr[i] );
-        tr[i].addEventListener('click', () => {});
-    })
+    
 }
 
 window.addEventListener("DOMContentLoaded", function () {
