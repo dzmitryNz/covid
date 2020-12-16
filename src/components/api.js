8707aff3744f1e6641c5f7222fac09b1f6d1a878
dzmitry nayezzhy <d.nayezzhy@gmail.com>
2020-12-16 12:03:24 +0300

    fix: fix lint problems

--- src/components/api.js
+++ src/components/api.js
@@ -1,42 +1,22 @@
+/* eslint-disable import/no-cycle */
+
 import * as importedApis from "../api/default.json";
 import Page from "../index";
-import listOfCountries from "./list-of-countries/js/list-of-countries";
+// import listOfCountries from "./list-of-countries/js/list-of-countries";
 // import chart from './chart/js/chart.js'
 // import map from './map/js/map.js'
 // import country-data from './country-data/js/country-data.js'
 
 const apiPaths = importedApis[0];
-const summary = "summaryRoute";
-const countries = "countriesRoute";
-const countryDay = "countryDayOneRoute";
-const countryTotalDay = "countryDayOneTotalRoute";
-const country = "ukraine";
+// const summary = "summaryRoute";
+// const countries = "countriesRoute";
+// const countryDay = "countryDayOneRoute";
+// const countryTotalDay = "countryDayOneTotalRoute";
+// const country = "ukraine";
 const summaryUrl = "https://api.covid19api.com/summary";
 const UpdatePeriod = 14;
 let url = "";
 let categoryData = {};
-
-export default async function getData(category, country) {
-  const date = new Date();
-  if (category) {
-    const baseUrl = apiPaths.baseUrl.Path;
-    let catUrl = apiPaths[category].Path;
-
-    if (catUrl.match(/:country/) && country) catUrl = catUrl.replace(/:country/, country);
-
-    url = baseUrl + catUrl;
-  } else { url = summaryUrl; category = "summaryRoute"; }
-
-  if (country) categoryData = JSON.parse(localStorage.getItem(`${category}-${country}`));
-  else { categoryData = JSON.parse(localStorage.getItem(category)); }
-
-  if (categoryData) {
-    const LastUpdate = new Date(categoryData.Date);
-    const deltaHours = new Date(date - LastUpdate).getHours();
-    if (deltaHours > UpdatePeriod) getApi(category, country);
-    else { getApi(category, country); }
-  } else { getApi(category, country); }
-}
 
 async function getApi(category, country) {
   const response = await fetch(url);
@@ -46,4 +26,27 @@ async function getApi(category, country) {
     else { localStorage.setItem(category, JSON.stringify(result)); }
     Page.set(result, category);
   } else { console.log(result.Message); }
+}
+
+export default async function getData(category, country) {
+  const date = new Date();
+  let categoryApi = category;
+  if (categoryApi) {
+    const baseUrl = apiPaths.baseUrl.Path;
+    let catUrl = apiPaths[categoryApi].Path;
+
+    if (catUrl.match(/:country/) && country) catUrl = catUrl.replace(/:country/, country);
+
+    url = baseUrl + catUrl;
+  } else { url = summaryUrl; categoryApi = "summaryRoute"; }
+
+  if (country) categoryData = JSON.parse(localStorage.getItem(`${categoryApi}-${country}`));
+  else { categoryData = JSON.parse(localStorage.getItem(categoryApi)); }
+
+  if (categoryData) {
+    const LastUpdate = new Date(categoryData.Date);
+    const deltaHours = new Date(date - LastUpdate).getHours();
+    if (deltaHours > UpdatePeriod) getApi(categoryApi, country);
+    else { getApi(categoryApi, country); }
+  } else { getApi(categoryApi, country); }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+/* eslint-disable import/no-cycle */
+
 import "./components/index.css";
 import getData from "./components/api";
 import listOfCountries from "./components/list-of-countries/js/list-of-countries";
 import { chart } from "./components/chart/js/chart";
 import Map from "./components/map/js/map";
 // import map from './components/map/js/map.js'
-// import country-data from './components/country-data/js/country-data.js'
+//  import country-data from './components/country-data/js/country-data.js'
 
 const summary = "summaryRoute";
-const countries = "countriesRoute";
-const countryDay = "countryDayOneRoute";
-const countryTotalDay = "countryDayOneTotalRoute";
-const country = "ukraine";
+// const countries = "countriesRoute";
+// const countryDay = "countryDayOneRoute";
+// const countryTotalDay = "countryDayOneTotalRoute";
+// const country = "ukraine";
 let Page = {};
 
 export default Page = {
@@ -27,7 +29,7 @@ export default Page = {
   },
 
   set(categoryData, category) {
-    listOfCountries(categoryData);
+    listOfCountries(categoryData, category);
     chart(categoryData);
     const map = new Map(categoryData);
     map.getMap();
